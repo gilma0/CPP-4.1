5ad7b159bded08ecf9487dbe7a2737969edd34d5
gilma0 <44779445+gilma0@users.noreply.github.com>
2019-05-02 12:41:30 +0300

    Add files via upload

--- calculate.cpp
+++ calculate.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include "calculate.hpp"
+using namespace std;
 
 namespace bullpgia{
-	string calculateBullAndPgia(string a, string b){
-		return "";
+	string calculateBullAndPgia(string choose, string guess){
+		int cSize = choose.length(); //////////remember to check if double digits
+		int gSize = guess.length();
+		int a = 0;
+		int b = 0;
+		int bull = 0;
+		int pgia = 0;
+		while (a < cSize){
+			char now = choose.at(a);
+			b = 0;
+			while (b < gSize){
+				if (guess.at(b) == now){
+					if (a == b){
+						bull++;
+					}
+					else{
+						pgia++;
+					}
+				}
+				b++;
+			}
+			a++;
+		}
+		string answer = to_string(bull)+","+to_string(pgia);
+		return answer;
 	}
 }
