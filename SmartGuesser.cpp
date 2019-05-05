59b3c86bd7df1ee82ac5666397fe4a5ac2a2c3b9
gilma0 <44779445+gilma0@users.noreply.github.com>
2019-05-05 17:33:16 +0300

    Update SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,21 +1,14 @@
 #include "SmartGuesser.hpp"
+#include <string>
 using namespace std;
 namespace bullpgia{
 	uint length;
-	//int pgiaA[4];
-	char bullA[999];
+	int bullA[4];
 	int nowBull;
-	//int nowPgia;
 	int now;
 	string Guess;
 	int bull;
-	//int pgia;
 	SmartGuesser::SmartGuesser(){
-		for (int i = 0; i<999; i++){
-			//pgiaA[i] = -1;
-			bullA[i] = 'd';
-		}
-		//pgia = 0;
 		bull = 0;
 		now = 0;
 		Guess = "";
@@ -25,65 +18,44 @@ namespace bullpgia{
 		return Guess;
 	}
 	void SmartGuesser::learn(string a){
-		nowBull = a.at(0);
-		//nowPgia = a.at(2);
-		if (now <= 9){
-			if (nowBull > bull){
-				bullA[bull] = (now+48);
-				bull++;
-				cout<<"this is changed char "<< (char)bullA[nowBull]<<endl;
-				cout<<"this is now "<<now<<endl;
-			}
-			now++;
-			return;
-		}
-		now = 0;
-		int h = 0;
-		Guess = "";
-		while (h<length){
-			Guess += to_string(bullA[h++]);
-		}
-		Guess += to_string(now++);
-/*		//nowPgia = a.at(2);
-		if (now <= 9){
-			if (nowBull > bull){
-				bullA[nowBull++] = now;
-				bull++;
-			}
-			if (nowPgia > pgia){
-				pgiaA[nowPgia++] = now;
-				pgia++;
-			}
-			now++;
-			return;
-		}
-		if
-		Guess += to_string(bullA[0]);
+		cout<<"learn used"<<endl;
+		nowBull = a.at(0)-48;
 		if (nowBull > bull){
-			bullA[nowBull-1] = now;
+			cout<<"now bull: "<<nowBull<<endl;
+			bullA[bull++] = now;
 			now = 0;
-			bull++;
+			Guess = "";
+			for (int i = 0; i<bull; i++){
+				Guess += to_string(bullA[i]);
+			}
+			if(Guess.length() < length){
+				Guess += to_string(now++);
+			}
+			while (Guess.length() < length){
+				Guess += 'd';
+			}
+		}else{
+			Guess = "";
+			for (int i = 0; i<bull; i++){
+				Guess += to_string(bullA[i]);
+			}
+			if(Guess.length() < length){
+				Guess += to_string(now++);
+			}
+			while (Guess.length() < length){
+				Guess += string(1,'d');
+			}
 		}
-		int h = 0;
-		Guess = "";
-		while (h<length){
-			Guess += to_string(bullA[h++]);
-		}
-		Guess += to_string(now++);*/
 	}
 	void SmartGuesser::startNewGame(int alength){
 		length = alength;
-		for (int i = 0; i<length; i++){
-			//pgiaA[i] = -1;
-			bullA[i] = 'd';
-		}
-		//pgia = 0;
 		bull = 0;
 		now = 0;
 		Guess = "";
 		int h = 0;
 		while (h<length){
-			Guess += string(1, bullA[h++]);
+			Guess += string(1,'d');
+			h++;
 		}
 	}
 }
